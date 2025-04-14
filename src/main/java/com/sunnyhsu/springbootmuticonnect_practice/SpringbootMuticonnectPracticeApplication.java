54d643553a106d0c0052bec7dd1e6bc582524e39
Chtistina326215 <christina326215@gmail.com>
2025-04-14 16:44:35 +0800

    muticonnect_practice

---
+++ src/main/java/com/sunnyhsu/springbootmuticonnect_practice/SpringbootMuticonnectPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sunnyhsu.springbootmuticonnect_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMuticonnectPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMuticonnectPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sunnyhsu/springbootmuticonnect_practice/SpringbootMuticonnectPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunnyhsu.springbootmuticonnect_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMuticonnectPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
